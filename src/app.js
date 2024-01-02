a14d1ce65dd64d09acb336162210645d53efea8f
Henrique De Antoni <147777120+henriquedeantoni@users.noreply.github.com>
2024-01-02 20:51:58 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const corsOptions = {
 class App {
   constructor() {
     this.app = express()
-    this.app.use(cors())
+    this.app.use(cors(corsOptions))
 
     this.middlewares()
     this.routes()
